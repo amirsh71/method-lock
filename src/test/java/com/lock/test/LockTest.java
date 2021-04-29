b93f6065d8261c8612ea573b5db5d1da34049982
omidp <qweASD@123>
2021-04-29 21:55:33 +0800

    add missing enableaspectjproxy annotation

--- src/test/java/com/lock/test/LockTest.java
+++ src/test/java/com/lock/test/LockTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
@@ -86,6 +87,7 @@ public class LockTest
 
     @ComponentScan(basePackages = "com.amirsh71.methodlock.core")
     @Configuration
+    @EnableAspectJAutoProxy
     public static class LockTestConfig
     {
         @Bean
